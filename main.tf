cea6dc892441668abdd13ee06db3651fda5886f1
Stephen Martin <stephen.martin@tedaisytech.com>
2023-07-05 20:24:43 +0100

    com

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "azurerm" {
-    version = "2.5.0"
-    features {}
+  version = "2.5.0"
+  features {}
 }
 
 terraform {
-    backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstoragebinarythistle2"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "tf_rg_blobstore"
+    storage_account_name = "tfstoragebinarythistle2"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 variable "imagebuild" {
@@ -20,28 +20,28 @@ variable "imagebuild" {
 
 
 resource "azurerm_resource_group" "tf_test" {
-  name = "tfmainrg"
+  name     = "tfmainrg"
   location = "Australia East"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
-  name                      = "weatherapi"
-  location                  = azurerm_resource_group.tf_test.location
-  resource_group_name       = azurerm_resource_group.tf_test.name
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
 
-  ip_address_type     = "public"
-  dns_name_label      = "binarythistlewa"
-  os_type             = "Linux"
+  ip_address_type = "public"
+  dns_name_label  = "binarythistlewa"
+  os_type         = "Linux"
 
   container {
-      name            = "weatherapi"
-      image           = "smartin43592/weatherapi:${var.imagebuild}"
-        cpu             = "1"
-        memory          = "1"
+    name   = "weatherapi"
+    image  = "smartin43592/weatherapi:${var.imagebuild}"
+    cpu    = "1"
+    memory = "1"
 
-        ports {
-            port        = 80
-            protocol    = "TCP"
-        }
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
   }
 }
